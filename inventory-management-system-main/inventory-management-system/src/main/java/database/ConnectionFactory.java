d0f24cec732028d388e6f647f028ff4c83c673b0
Kore Rakesh <korerakesh2004@gmail.com>
2025-09-07 17:16:55 +0530

    Update ConnectionFactory.java

--- inventory-management-system-main/inventory-management-system/src/main/java/database/ConnectionFactory.java
+++ inventory-management-system-main/inventory-management-system/src/main/java/database/ConnectionFactory.java
@@ -14,7 +14,7 @@ public class ConnectionFactory {
             throw new RuntimeException(e);
         }
     }
-    
+
     
     public static ConnectionFactory getInstance(){
         ConnectionFactory result = instance;
